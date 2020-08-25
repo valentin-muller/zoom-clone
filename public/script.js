19f56f28cb321377983a028fe323d5badd3eaf1b
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-08-25 16:35:52 +0200

    Peer.js New user stream and show video progress

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+/* YOUTUBE from 1:25h to solve this part
 let myVideoStream;
-
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -12,7 +18,33 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
+*/
+
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io = require('socket.io')(server)
 const { v4: uuidv4 } = require("uuid");
-app.set("view engine", "ejs");
 
+const { ExpressPeerServer } = require('peer');
+const { truncate } = require("fs");
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use('/peerjs', peerServer);
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
@@ -13,5 +21,12 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    })
+})
 
 server.listen(3030);
